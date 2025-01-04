03d1f27c71de07f00ce23c7c1efb1c656b8f1757
SVinothan <svinothan09@gmail.com>
2025-01-04 15:43:44 +0530

    city model and migration and seeder created

---
+++ database/migrations/2025_01_04_153232_create_cities_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->integer('province_id')->nullable();
+            $table->integer('district_id')->nullable();
+            $table->string('name_en')->nullable();
+            $table->string('name_si')->nullable();
+            $table->string('name_ta')->nullable();
+            $table->string('sub_name_en')->nullable();
+            $table->string('sub_name_si')->nullable();
+            $table->string('sub_name_ta')->nullable();
+            $table->string('postcode')->nullable();
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+};
