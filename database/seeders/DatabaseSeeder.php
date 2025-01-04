40e8dc253d3a83ae8913360dc7305a65eb4ec3c0
SVinothan <svinothan09@gmail.com>
2025-01-04 15:46:23 +0530

    spartie package installed for permissions handling

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,26 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => '$2y$12$Gf2YjItx06rrJyWufikHd.bekB5TUyPDN466h37ruZxIbjaxfRqLm', //12345678
+            'role' => 'SuperAdmin', 
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'SuperAdmin',
+            'guard_name' => 'web',
+        ]);
+
+       
+        $user=User::find(1);
+        $user->assignRole('SuperAdmin');
+
+        $this->call(CitiesTableSeeder::class);
+        $this->call(DistrictsTableSeeder::class);
+        $this->call(ProvincesTableSeeder::class);
+
     }
 }
