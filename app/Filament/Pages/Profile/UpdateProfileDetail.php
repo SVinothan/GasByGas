acb58451c3dd7c9dad3138e50789119961ccea2a
SVinothan <svinothan09@gmail.com>
2025-01-05 21:37:06 +0530

    change profile details page created

---
+++ app/Filament/Pages/Profile/UpdateProfileDetail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages\Profile;
+
+use Filament\Pages\Page;
+
+class UpdateProfileDetail extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.profile.update-profile-detail';
+}
