c9b7339899f0e08d678ebeedd878dfbcba8dd683
SVinothan <svinothan09@gmail.com>
2025-01-05 21:36:43 +0530

    base route and cache clear route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+   return redirect()->route('filament.admin.auth.login');
+   
 });
+
+Route::get('clear-cache',function(){
+    $run = Artisan::call('cache:clear');
+    $run = Artisan::call('config:clear');
+    $run = Artisan::call('config:cache');
+
+    return redirect()->route('filament.admin.pages.dashboard');
+})->name('filament.clear.cache');
