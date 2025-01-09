177aac54ced95eb22095d3fe28b9bc6f2dbe49dc
SVinothan <svinothan09@gmail.com>
2025-01-09 19:46:36 +0530

    add new model

---
+++ database/migrations/2025_01_09_193341_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('name')->nullable();
+            $table->decimal('height_in_meter')->default('0.00');
+            $table->decimal('weight_in_kg')->default('0.00');
+            $table->decimal('capacity')->default('0.00');
+            $table->string('color')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
