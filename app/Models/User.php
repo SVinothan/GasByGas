72d68ac6670974c253e2145fad471c649bb8a790
SVinothan <svinothan09@gmail.com>
2025-01-17 06:32:36 +0530

    login permission based on user status

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,17 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return ($this->is_active);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +29,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'customer_id',
+        'employee_id',
+        'is_active',
     ];
 
     /**
