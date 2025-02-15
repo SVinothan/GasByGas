5b7952b7787056c624e9de141adc3134ece2a931
SVinothan <svinothan09@gmail.com>
2025-02-15 17:17:23 +0530

    customer order widget based on roles

--- app/Filament/Widgets/LatestCustomerOrders.php
+++ app/Filament/Widgets/LatestCustomerOrders.php
@@ -11,28 +11,74 @@ class LatestCustomerOrders extends BaseWidget
 {
     public function table(Table $table): Table
     {
-        return $table
-            ->query(
-                CustomerOrder::where('status','Order Pending')->orderBy('updated_at','desc')->limit(5)
-            )
-            ->columns([
-                Tables\Columns\TextColumn::make('customerOrderCity.name_en')
-                    ->label('City Name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('customerOrderOutlet.outlet_name')
-                    ->label('Outlet Name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('customerOrderCustomer.full_name')
-                    ->label('Customer Name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('status')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('token_no')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('order_date')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('pickup_date')
-                    ->searchable(),
-            ]);
+        if(auth()->user()->getRoleNames()->first() == 'Customer')
+        {
+            return $table
+                ->query(
+                    CustomerOrder::where('status','Order Pending')->where('customer_id',auth()->user()->customer_id)->orderBy('updated_at','desc')->limit(5)
+                )
+                ->columns([
+                    Tables\Columns\TextColumn::make('customerOrderOutlet.outlet_name')
+                        ->label('Outlet Name')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('status')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('token_no')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('order_date')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('pickup_date')
+                        ->searchable(),
+                ]);
+        }
+        else if(auth()->user()->getRoleNames()->first() == 'OutletManager')
+        {
+            return $table
+                ->query(
+                    CustomerOrder::where('status','Order Pending')->where('outlet_id',auth()->user()->userEmployee->outlet_id)->orderBy('updated_at','desc')->limit(5)
+                )
+                ->columns([
+                    Tables\Columns\TextColumn::make('customerOrderCustomer.full_name')
+                        ->label('Customer Name')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('customerOrderCustomer.mobile_no')
+                        ->label('Customer Name')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('status')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('token_no')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('order_date')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('pickup_date')
+                        ->searchable(),
+                ]);
+        }
+        else
+        {
+            return $table
+                ->query(
+                    CustomerOrder::where('status','Order Pending')->orderBy('updated_at','desc')->limit(5)
+                )
+                ->columns([
+                    Tables\Columns\TextColumn::make('customerOrderCity.name_en')
+                        ->label('City Name')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('customerOrderOutlet.outlet_name')
+                        ->label('Outlet Name')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('customerOrderCustomer.full_name')
+                        ->label('Customer Name')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('status')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('token_no')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('order_date')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('pickup_date')
+                        ->searchable(),
+                ]);
+        }
     }
 }
