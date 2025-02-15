6163642b93fa4f0430b089efb52992ca517e5ca7
SVinothan <svinothan09@gmail.com>
2025-02-15 17:23:11 +0530

    scheduled delivery widget based on roles

--- app/Filament/Widgets/LatestScheduleDelivery.php
+++ app/Filament/Widgets/LatestScheduleDelivery.php
@@ -11,27 +11,71 @@ class LatestScheduleDelivery extends BaseWidget
 {
     public function table(Table $table): Table
     {
-        return $table
-            ->query(
-                ScheduleDelivery::where('status','Scheduled')->orderBy('updated_at','desc')->limit(5)
-            )
-            ->columns([
-                Tables\Columns\TextColumn::make('scheduleDeliveryCity.name_en')
-                    ->label('City Name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('scheduleDeliveryOutlet.outlet_name')
-                    ->label('Outlet Name')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('schedule_no')
-                    ->label('Schedule Code')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('scheduled_date')
-                    ->date()
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('no_of_item')
-                    ->searchable()->label('Items'),
-                Tables\Columns\TextColumn::make('no_of_qty')
-                    ->searchable()->label('Qtys'),
-            ]);
+        if(auth()->user()->getRoleNames()->first() == 'Customer')
+        {
+            return $table
+                ->query(
+                    ScheduleDelivery::where('status','Scheduled')->where('city_id',auth()->user()->userCustomer->city_id)->orderBy('updated_at','desc')->limit(5)
+                )
+                ->columns([
+                    Tables\Columns\TextColumn::make('scheduleDeliveryOutlet.outlet_name')
+                        ->label('Outlet Name')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('schedule_no')
+                        ->label('Schedule Code')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('scheduled_date')
+                        ->date()
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('no_of_item')
+                        ->searchable()->label('Items'),
+                    Tables\Columns\TextColumn::make('no_of_qty')
+                        ->searchable()->label('Qtys'),
+                ]);
+        }
+        else if(auth()->user()->getRoleNames()->first() == 'OutletManager')
+        {
+            return $table
+                ->query(
+                    ScheduleDelivery::where('status','Scheduled')->where('outlet_id',auth()->user()->userEmployee->outlet_id)->orderBy('updated_at','desc')->limit(5)
+                )
+                ->columns([
+                    Tables\Columns\TextColumn::make('schedule_no')
+                        ->label('Schedule Code')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('scheduled_date')
+                        ->date()
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('no_of_item')
+                        ->searchable()->label('Items'),
+                    Tables\Columns\TextColumn::make('no_of_qty')
+                        ->searchable()->label('Qtys'),
+                ]);
+        }
+        else
+        {
+            return $table
+                ->query(
+                    ScheduleDelivery::where('status','Scheduled')->orderBy('updated_at','desc')->limit(5)
+                )
+                ->columns([
+                    Tables\Columns\TextColumn::make('scheduleDeliveryCity.name_en')
+                        ->label('City Name')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('scheduleDeliveryOutlet.outlet_name')
+                        ->label('Outlet Name')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('schedule_no')
+                        ->label('Schedule Code')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('scheduled_date')
+                        ->date()
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('no_of_item')
+                        ->searchable()->label('Items'),
+                    Tables\Columns\TextColumn::make('no_of_qty')
+                        ->searchable()->label('Qtys'),
+                ]);
+        }
     }
 }
